f62fcd7cde74d0c437e63514f240662c17d6159c
kahmassensini <karinneamp22@gmail.com>
2022-01-13 16:13:44 -0300

    first-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './views/shared/top-bar/top-bar.component';
+import { FeedComponent } from './views/pages/feed/feed.component';
+import { PostComponent } from './views/shared/post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    FeedComponent,
+    PostComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
